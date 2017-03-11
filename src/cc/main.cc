6aede350b56894007760e0c3aae218d580498ab5
Arty Buldauskas <abuldauskas@wayfair.com>
2017-03-11 16:59:50 -0500

    test out... boost-test

---
+++ src/cc/main.cc
@@ -0,0 +1,8 @@
+#include <cstdio>
+
+int main(void) {
+  printf("Hello World!");
+
+  return 0;
+}
+
---
+++ src/cc/tests/main.cc
@@ -0,0 +1,10 @@
+#define BOOST_TEST_MODULE Main
+#include <boost/test/included/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(first_test)
+{
+  int i = 1;
+  BOOST_TEST(i);
+  BOOST_TEST(i == 2);
+}
+
